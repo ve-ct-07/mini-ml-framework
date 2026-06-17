34916d65c85506a4190931d4a71bbfec0b102e32
Charan Teja <cherrycharantej112@gmail.com>
2026-06-17 12:19:21 +0530

    Add main entry point script

---
+++ mini_ml/__init__.py
@@ -0,0 +1,29 @@
+"""
+my_ml_lib: A foundational machine learning library built using NumPy.
+
+Provides implementations of:
+- Linear Models
+- Naive Bayes
+- Data Preprocessing
+- Model Selection Utilities
+- Neural Networks
+"""
+
+__version__ = "1.0.0"
+
+# Import subpackages
+from . import datasets
+from . import linear_models
+from . import model_selection
+from . import naive_bayes
+from . import preprocessing
+from . import nn
+
+__all__ = [
+    "datasets",
+    "linear_models",
+    "model_selection",
+    "naive_bayes",
+    "preprocessing",
+    "nn",
+]
